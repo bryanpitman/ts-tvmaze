0b52308fa3b7a7871a7a4f7a56d37e84045973c1
Jordan Wong <pjwong97@gmail.com>
2022-09-12 16:37:50 -0700

    complete some fixes for interfaces

--- tvmaze.ts
+++ tvmaze.ts
@@ -5,11 +5,25 @@ const $showsList = $("#showsList");
 const $episodesArea = $("#episodesArea");
 const $searchForm = $("#searchForm");
 
-interface showInterface {
+interface ShowInterface {
   id: number;
   name: string;
   summary: string;
   image: string;
+}
+
+interface ShowFromApiInterface {
+  id: number;
+  name: string;
+  summary: string;
+  image: { medium: string; } | null;
+}
+
+interface EpisodeInterface {
+  id: number;
+  name: string;
+  season: number;
+  number: number;
 }
 
 /** Given a search term, search for tv shows that match that query.
@@ -19,38 +33,44 @@ interface showInterface {
  *    (if no image URL given by API, put in a default image URL)
  */
 
-async function getShowsByTerm(term: string): Promise<Object[]> {
+async function getShowsByTerm(term: string): Promise<ShowInterface[]> {
   // ADD: Remove placeholder & make request to TVMaze search shows API.
-  const response : Object[] = await axios.get(
+  const response = await axios.get(
     `https://api.tvmaze.com/search/shows?q=${term}`,
-  )
-    console.log(response)
+  );
 
-    return response.data.map((show): showInterface => {
-      {
-        id: show.show.id,
-        name: show.show.name,
-        summary: show.show.summary,
-        image: show.show.image.medium,
-    }
-  }
+  // console.log("response.data", response.data);
+  // console.log("img is", response.data[0].show.image);
+  // console.log("img medium is", response.data[0].show.image.medium);
 
+  return response.data.map((show: { show: ShowFromApiInterface; }) => {
+    //var show in map is of type object with key of show
+    return {
+      id: show.show.id,
+      name: show.show.name,
+      summary: show.show.summary,
+      image: show.show.image?.medium || "http://tinyurl.com/tv-missing"
+      // image: show.show.image
+      //   ? show.show.image.medium
+      //   : "http://tinyurl.com/tv-missing"
+    };
+
+  });
 
 }
 
+/** Given list of shows, create markup for each and append to DOM */
 
-/** Given list of shows, create markup for each and to DOM */
-
-function populateShows(shows) {
+function populateShows(shows: ShowInterface[]) {
   $showsList.empty();
 
   for (let show of shows) {
     const $show = $(
-        `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
+      `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
          <div class="media">
            <img
-              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg"
-              alt="Bletchly Circle San Francisco"
+              src=${show.image}
+              alt="http://tinyurl.com/tv-missing"
               class="w-25 me-3">
            <div class="media-body">
              <h5 class="text-primary">${show.name}</h5>
@@ -63,7 +83,8 @@ function populateShows(shows) {
        </div>
       `);
 
-    $showsList.append($show);  }
+    $showsList.append($show);
+  }
 }
 
 
@@ -72,8 +93,8 @@ function populateShows(shows) {
  */
 
 async function searchForShowAndDisplay() {
-  const term = $("#searchForm-term").val();
-  const shows = await getShowsByTerm(term);
+  const term = $("#searchForm-term").val() as string;
+  const shows: ShowInterface[] = await getShowsByTerm(term);
 
   $episodesArea.hide();
   populateShows(shows);
@@ -84,13 +105,52 @@ $searchForm.on("submit", async function (evt) {
   await searchForShowAndDisplay();
 });
 
+//TODO: jquery for episodes button to handle click?
 
 /** Given a show ID, get from API and return (promise) array of episodes:
  *      { id, name, season, number }
  */
 
-// async function getEpisodesOfShow(id) { }
+async function getEpisodesOfShow(id: number): Promise<EpisodeInterface[]> {
+  const response = await axios.get(
+    `https://api.tvmaze.com/shows/${id}/episodes`,
+  );
 
-/** Write a clear docstring for this function... */
+  const show = response.data;
 
-// function populateEpisodes(episodes) { }
+  return response.data.map((episode: { episode: EpisodeInterface; }) => {
+    return {
+      id: show.id,
+      name: show.name,
+      season: show.season,
+      number: show.number.medium
+    };
+  }
+  );
+
+}
+
+/** Given list of episodes, create markup for each and append to DOM */
+
+function populateEpisodes(episodes: EpisodeInterface[]) {
+  $episodesArea.empty();
+
+  for (let episode of episodes) {
+    const $episode = $(
+      `<div data-episode-id="${episode.id}" class="Episode col-md-12 col-lg-6 mb-4">
+         <div class="media">
+           <div class="media-body">
+             <h5 class="text-primary">${episode.name}</h5>
+             <div><small>${episode.season}</small></div>
+             <div><small>${episode.number}</small></div>
+             <button class="btn btn-outline-light btn-sm Episode-getEpisodes">
+               Episodes
+             </button>
+           </div>
+         </div>
+       </div>
+      `);
+
+    $episodesArea.append($episode);
+  }
+}
